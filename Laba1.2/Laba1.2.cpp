ed7c919b3869f0033f0b59f2a1c02bf442cb5891
Асафьев Александр <asafjew2015@yandex.ru>
2020-04-29 23:53:05 +0300

    Добавьте файлы проекта.

---
+++ Laba1.2/Laba1.2.cpp
@@ -0,0 +1,43 @@
+﻿#include<cstdlib> 
+#include<iostream> 
+#include<math.h> 
+using namespace std;
+
+float F1(int& x)
+{
+	float result;
+	if (x >= 0)
+	{
+		result = sin(x);
+	}
+	else
+	{
+		result = cos(x);
+	}
+	return result;
+}
+void F2(int& x)
+{
+	x = F1(x);
+}
+
+
+int main()
+{
+	int N;
+	cin >> N;
+	for (int i = -3; i < N; i++)
+	{
+		float f = F1(i);
+		cout << "i=" << i << "; F1(i)=" << f << endl;
+	}
+	cout << " " << endl;
+	for (int i = -3; i < N; i++)
+	{
+		cout  "i=" << i << ";";
+		F2(i);
+		cout << "i=" << i << endl;
+	}
+	system("pause>null");
+	return 0;
+}
