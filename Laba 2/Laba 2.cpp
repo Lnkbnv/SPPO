dc862278d26b91f796b70135b9312bf05bfa28f5
Асафьев Александр <asafjew2015@yandex.ru>
2020-04-30 15:19:30 +0300

    Добавьте файлы проекта.

---
+++ Laba 2/Laba 2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	cout << "Введите размер массива=";
+	cin >> n;
+	int* mas = new int[n];
+	cout << "Введите элементы массива:" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> mas[i];
+	}
+	int temp;
+
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = 0; j < n - i - 1; j++)
+		{
+			if (mas[j] > mas[j + 1])
+			{
+
+				temp = mas[j];
+				mas[j] = mas[j + 1];
+				mas[j + 1] = temp;
+			}
+		}
+	}
+
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << mas[i] << "";
+	}
+	cout << endl;
+
+	delete[] mas;
+
+	return 0;
+}
+
